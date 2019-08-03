caa15daf80a21a513b5069b955bc2b2f1ffe7bf9
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-08-03 15:36:47 +0530

    PRODUT DETAIL

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,18 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product =>{
+    res.render('shop/product-detail', 
+                {
+                  product: product,
+                  pageTitle: product.title,
+                  path:'/products'
+                })
+  })
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -40,3 +52,5 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
+
